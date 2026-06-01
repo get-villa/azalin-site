50b8284d994e91a9695a44395bfc1811be9a5541
Joshua Allen <joshallen647@gmail.com>
2026-06-01 13:56:25 -0400

    Add astro-robots-txt integration and llms.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://azalin.io',
 	output: 'static',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(),
+		sitemap(),
+		robotsTxt(),
+	],
 });
